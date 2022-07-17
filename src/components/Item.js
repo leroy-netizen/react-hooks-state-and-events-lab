d6ef7f69ae59faf00ea6f84db14a74076d9ae418
Leroy-netizen <leroythomas051@gmail.com>
2022-07-17 20:09:43 +0300

    commit tree 8db38a877bb81574c6a97322e57a695574ded7a5

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState("");
+
+  const addToCart = (e) => {
+    setInCart("in-cart");
+    console.log(inCart);
+  };
+  let cartClass = inCart === "in-cart" ? "in-cart" : "";
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>
+        Add to Cart
+      </button>
     </li>
   );
 }
